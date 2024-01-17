cdc17a7b066874dd704a72fbe458540ad947cacf
saurabh <saurabhtripathi147@gmail.com>
2024-01-17 13:04:00 +0530

    put get me api created

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -29,7 +29,45 @@ userRouter.post('/login', async (req : Request, res : Response)=>{
 userRouter.post('/register', async(req : Request, res : Response)=>{
      const body = req.body
      const existingUser = await User.findOne({email: body.email})
+     if(!existingUser){
+        const user = await User.create(body)
+        res.status(200).json({
+            message : 'user created successfully',
+            user : {
+                id : user._id,
+                name : user.name,
+                email : user.email
+            }
+        })
+     }
+     else{
+         res.status(400).json({message:"user already exists"})
+     }
 })
+
+// userRouter.get('/me', async (req : Request, res : Response)=>{
+//     res.json(req.context.user)
+// })
+
+userRouter.get('/:id', async(req : Request, res : Response)=>{
+    const id = req.params.id
+    const user = await User.findById(id)
+    res.json(user)
+})
+
+userRouter.put('/:id', async(req : Request, res : Response)=>{
+    const id = req.params.id
+    const {email , password} = req.body
+
+    const newEntry = {
+        email, password
+    }
+
+    const user = User.findByIdAndUpdate(newEntry)
+    res.json(user)
+})
+
+
 
 
 export default userRouter
