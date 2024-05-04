f966041c785c218d6a633ed6ea0b719520594524
saurabh <siddharthtripathi063@gmail.com>
2024-05-04 21:07:04 +0530

    revert to old config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    // Use the clearScreen option to disable ESLint error output during build
-    
-  },
-});
+})
